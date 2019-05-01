8d560b339a8e710b3b06a21451f1caa9d068ce8d
Zhenyun Yu <me@yuzhenyun.me>
2019-05-02 00:43:18 +0800

    add fields for api object && gen code

--- pkg/apis/serving/v1/tfinference_types.go
+++ pkg/apis/serving/v1/tfinference_types.go
@@ -13,6 +13,7 @@ type TfInferenceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	Size int32 `json:"size"`
 }
 
 // TfInferenceStatus defines the observed state of TfInference
@@ -21,6 +22,7 @@ type TfInferenceStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	Nodes []string `json:"nodes"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
