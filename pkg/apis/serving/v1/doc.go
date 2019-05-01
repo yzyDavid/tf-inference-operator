6baa2f22b64e96ece1db147d90fc082dfedcb3be
Zhenyun Yu <me@yuzhenyun.me>
2019-05-02 00:33:12 +0800

    back to dep && adding api

---
+++ pkg/apis/serving/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the serving v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=serving.example.com
+package v1
---
+++ pkg/apis/serving/v1/tfinference_types.go
@@ -0,0 +1,49 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TfInferenceSpec defines the desired state of TfInference
+// +k8s:openapi-gen=true
+type TfInferenceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// TfInferenceStatus defines the observed state of TfInference
+// +k8s:openapi-gen=true
+type TfInferenceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TfInference is the Schema for the tfinferences API
+// +k8s:openapi-gen=true
+type TfInference struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TfInferenceSpec   `json:"spec,omitempty"`
+	Status TfInferenceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TfInferenceList contains a list of TfInference
+type TfInferenceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TfInference `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TfInference{}, &TfInferenceList{})
+}
