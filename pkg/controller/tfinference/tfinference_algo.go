d81b9e9e246d161b70e05fc3cd6f1d4d132886e0
Abracax <AbraxasAmI@gmail.com>
2019-05-13 03:04:45 +0800

    algorithm written, not debuggged

--- pkg/controller/tfinference/tfinference_algo.go
+++ pkg/controller/tfinference/tfinference_algo.go
@@ -3,12 +3,13 @@ package tfinference
 import (
 	servingv1 "github.com/yzyDavid/tf-inference-operator/pkg/apis/serving/v1"
 	"hash/fnv"
+	"sort"
 )
 
 type DeploymentMeta struct {
-	Hash uint32 `json:"hash"`
-	Models []string `json:"models"`
-	Replicas int32 `json:"replicas"`
+	Hash     uint32   `json:"hash"`
+	Models   []string `json:"models"`
+	Replicas int32    `json:"replicas"`
 }
 
 func getHashByModels(models []string) uint32 {
@@ -16,21 +17,75 @@ func getHashByModels(models []string) uint32 {
 	for _, model := range models {
 		_, err := hasher.Write([]byte(model))
 		if err != nil {
-            // TODO
+			// TODO
 		}
 	}
 	return hasher.Sum32()
 }
 
+func removeModel(s []servingv1.Model, i int) []servingv1.Model {
+	return append(s[:i], s[i+1:]...)
+}
+
+func removeNode(s []servingv1.Node, i int) []servingv1.Node {
+	return append(s[:i], s[i+1:]...)
+}
+
+func deploymentInSlice(a DeploymentMeta, list []DeploymentMeta) bool {
+	for _, b := range list {
+		if b.Hash == a.Hash {
+			return true
+		}
+	}
+	return false
+}
+
 func getDeploymentMetas(models []servingv1.Model, nodes []servingv1.Node) []DeploymentMeta {
 	// TODO: real impl
-	var deployment DeploymentMeta
-	var names []string
-    for _, model := range models {
-		names = append(names, model.Name)
+	deployments := make([]DeploymentMeta, 0)
+
+	sort.SliceStable(models, func(i, j int) bool {
+		return models[i].Memory > models[j].Memory
+	})
+	sort.SliceStable(nodes, func(i, j int) bool {
+		return nodes[i].Memory > nodes[j].Memory
+	})
+	nodesLeft := make([]servingv1.Node, 0)
+	modelsLeft := make([]servingv1.Model, 0)
+	copy(modelsLeft, models)
+	copy(nodesLeft, nodes)
+	for len(nodesLeft) > 0 {
+		var deployment DeploymentMeta
+		names := make([]string, 0)
+		for i := 0; i < len(modelsLeft); i++ {
+			nodesLeft[0].Memory -= modelsLeft[i].Memory
+			nodesLeft[0].ComputingResource -= modelsLeft[i].ComputingResource
+			if nodesLeft[0].Memory < 0 {
+				nodesLeft[0].Memory += modelsLeft[i].Memory
+				continue
+			}
+			if nodesLeft[0].ComputingResource < 0 {
+				nodesLeft[0].ComputingResource += modelsLeft[i].ComputingResource
+				continue
+			} else {
+				names = append(names, modelsLeft[i].Name)
+				removeModel(modelsLeft, i)
+				i--
+			}
+		}
+		deployment.Models = names
+		deployment.Hash = getHashByModels(names)
+		if deploymentInSlice(deployment, deployments) {
+			for _, dep := range deployments {
+				if dep.Hash == deployment.Hash {
+					dep.Replicas++
+				}
+			}
+		} else {
+			deployment.Replicas = 1
+			deployments = append(deployments, deployment)
+		}
+		removeNode(nodesLeft, 0)
 	}
-    deployment.Models = names
-    deployment.Hash = getHashByModels(names)
-    deployment.Replicas = 3
-    return []DeploymentMeta{deployment}
+	return deployments
 }
